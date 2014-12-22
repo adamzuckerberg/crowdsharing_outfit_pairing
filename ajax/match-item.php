85b6b733cd55886e6dc395dd11a507ce7ed08955
adamzuckerberg <adam@adamzuckerberg.com>
2014-12-21 22:40:20 -0800

    Update media query to center item image

--- ajax/match-item.php
+++ ajax/match-item.php
@@ -7,7 +7,6 @@ if ($match_item == 10) {
 	$match_item = 0;
 }
 
-
 $new_item = $connection -> query("SELECT id, image FROM matches WHERE id = ".($match_item+1)." LIMIT 1");
 $connection -> close();
 $array_new_item = $new_item -> fetch_assoc();
